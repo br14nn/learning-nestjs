e20d268d47296abf327448f00792bbc1bf9c911b
br14nn <vituallabrian727@gmail.com>
2024-10-10 23:02:06 +0800

    learning C#

---
+++ learning-c-sharp/Giraffe/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //STRINGS
+            string phrase = "Giraffe Academy";
+
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase + "is cool!");
+
+            Console.ReadLine();
+
+            //DATATYPES
+            //string phase = "Giraffe Academy";
+            //char grade = 'C';
+            //int age = 30;
+            //double gpa = 3.3;
+            //bool isMale = true;
+
+            //Console.WriteLine(phase);
+
+            //Console.ReadLine();
+
+            //VARIABLES
+            //string characterName = "Dodong";
+            //int characterAge = 25;
+
+            //Console.WriteLine("There once was a man named " + characterName);
+            //Console.WriteLine("He was " + characterAge + " years old");
+            //Console.WriteLine("He really liked the name " + characterName);
+            //Console.WriteLine("But didn't like being" + characterAge);
+
+            //Console.ReadLine();
+        }
+    }
+}
