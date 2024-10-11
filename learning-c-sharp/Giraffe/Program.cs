005f0f76aa1ee98c029a49e97d87ff5b26d8b7f6
br14nn <vituallabrian727@gmail.com>
2024-10-11 16:48:57 +0800

    updated learning C#

--- learning-c-sharp/Giraffe/Program.cs
+++ learning-c-sharp/Giraffe/Program.cs
@@ -10,16 +10,56 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
+            //BUILDING A CALCULATOR
+            //Console.Write("Enter a number: ");
+            //double num1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter another number: ");
+            //double num2 = Convert.ToDouble((Console.ReadLine()));
+
+            //double sum = num1 + num2;
+            //Console.WriteLine("sum is: " + sum);
+
+            //Console.ReadLine();
+
+            //GETTING USER INPUT
+            //string name;
+            //string age;
+
+            //Console.Write("What is your name? ");
+            //name = Console.ReadLine();
+
+            //Console.Write("Your age: ");
+            //age = Console.ReadLine();
+
+            //Console.WriteLine("Hello " + name + ", you are " + age + " years old.");
+
+            //Console.ReadLine();
+
+            //NUMBERS
+            //Console.WriteLine(Math.Round(4.6));
+            //Console.WriteLine(Math.Sqrt(4));
+            //Console.WriteLine(Math.Pow(3.4, 2));
+            //Console.WriteLine(Math.Abs(-40));
+
+            //int num = 6;
+            //num++;
+            //Console.WriteLine(num);
+
+            //Console.ReadLine();
+
             //STRINGS
-            string phrase = "Giraffe Academy";
+            //string phrase = "Giraffe Academy";
 
-            Console.WriteLine(phrase.Contains("Academy"));
-            Console.WriteLine(phrase.ToLower());
-            Console.WriteLine(phrase.ToUpper());
-            Console.WriteLine(phrase.Length);
-            Console.WriteLine(phrase + "is cool!");
+            //Console.WriteLine(phrase.Substring(8));
+            //Console.WriteLine(phrase.IndexOf('f'));
+            //Console.WriteLine(phrase.Contains("Academy"));
+            //Console.WriteLine(phrase.ToLower());
+            //Console.WriteLine(phrase.ToUpper());
+            //Console.WriteLine(phrase.Length);
+            //Console.WriteLine(phrase + "is cool!");
 
-            Console.ReadLine();
+            //Console.ReadLine();
 
             //DATATYPES
             //string phase = "Giraffe Academy";
